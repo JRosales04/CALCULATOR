e2b320287e77bc91b4c4a5b2b05386531db027d5
100495802 <100495802@alumnos.uc3m.es>
2024-06-27 12:21:21 +0200

    Funcionamiento de botones numéricos y botón de borrado; ampliación de constantes con las posiciones de cada botón

--- src/main/python/calculator/constants/style.py
+++ src/main/python/calculator/constants/style.py
@@ -2,4 +2,30 @@
 Módulo que almacena las constantes del proyecto
 """
 
+import string
+
+# Tamaño de la matriz de widgets
+FILAS = 6
+COLUMNAS = 4
+
+# Longitud de número máxima
+LONGITUD_MAXIMA = 10
+
 BACKGROUND = "#084050"
+
+# Lista de dígitos numéricos
+digitos_numericos = list(string.digits)
+DIGITOS = digitos_numericos + [","]
+
+# Lista de operadores
+OPERADORES = ["+", "-", "*", "/", "^", "%", "√"]
+
+# Tamaño de los widgets
+TAMAÑO_BOTON = (1,1)
+TAMAÑO_ETIQUETA = (10,1)
+
+# Listas de tuplas para las posiciones de los botones
+POSICIONES_NUMEROS = [(5,1), (4,0), (4,1), (4,2), (3,0), (3,1), (3,2), (2,0), (2,1), (2,2), (5,0)]
+POSICIONES_OPERADORES = [(2,3), (3,3), (4,3), (5,3), (1,0), (1,1), (1,2)]
+POSICION_IGUAL = (5,2)
+POSICION_BORRADOR = (1,3)
--- src/main/python/calculator/screen.py
+++ src/main/python/calculator/screen.py
@@ -1,9 +1,14 @@
 """
 Módulo que maneja el funcionamiento de la interfaz,
 visual y lógicamente
+
+Este módulo permite expandir el funcionamiento de la
+aplicación, permitiendo la implementación de más
+pantallas (frames)
 """
 
 import tkinter as tk
+import string
 from constants import style
 
 class Screen(tk.Frame):
@@ -14,35 +19,80 @@ class Screen(tk.Frame):
         self.configure(background=style.BACKGROUND)
         self.controller = controller
         self.buttons = []
+        self.text_label = ""
         self.init_widgets()
 
     def init_widgets(self):
         """Método que inicializa los widgets"""
         self.init_label()
         self.init_buttons()
+        self.configure_grid()
+
+    def init_buttons(self):
+        """Método que inicializa los botones funcionales"""
+        self.init_numbers()
+        self.init_operators()
+        self.init_clear()
+        self.init_equal()
 
     def init_label(self):
         """Inicializa la etiqueta de texto"""
         # Crear la etiqueta grande
-        self.etiqueta = tk.Label(self, text="", font=("Arial", 20), width=10, height=1, bg="white", fg="black")
+        self.etiqueta = tk.Label(self, text=self.text_label, font=("Arial", 20), width=style.TAMAÑO_ETIQUETA[0],
+                           height=style.TAMAÑO_ETIQUETA[1], bg="white", fg="black")
         # Configurar la etiqueta en la cuadrícula
         self.etiqueta.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=2, pady=2)
         # Configurar la fila 0 para que se expanda
         self.grid_rowconfigure(0, weight=1)
         # Configurar las columnas para que se expandan
-        for col in range(4):
+        for col in range(style.COLUMNAS):
             self.grid_columnconfigure(col, weight=1)
 
-    def init_buttons(self):
-        """Inicializa los botones"""
-        # Crear una matriz de botones de 4x6 (24 botones en total)
-        contador = 0
-        for i in range(6):
-            for j in range(4):
-                button = tk.Button(self, text=str(contador), relief="flat", width=1, height=1)
-                button.grid(row=i + 1, column=j, sticky="nsew", padx=2, pady=2)
-                contador += 1
+    def init_numbers(self):
+        """Inicializa los botones para los números"""
+        for i in range(len(style.DIGITOS)):
+            button = tk.Button(self, text=style.DIGITOS[i], relief="flat", width=style.TAMAÑO_BOTON[0],
+                           height=style.TAMAÑO_BOTON[1], command=lambda t=style.DIGITOS[i]: self.write(t))
+            button.grid(row=style.POSICIONES_NUMEROS[i][0], column=style.POSICIONES_NUMEROS[i][1],
+                        sticky="nsew", padx=2, pady=2)
+            self.buttons.append(button)
 
-        # Configurar las filas 1 a 6 para que se expandan
-        for row in range(1, 7):
+    def init_operators(self):
+        """Inicializa los botones para los números"""
+        for i in range(len(style.OPERADORES)):
+            button = tk.Button(self, text=style.OPERADORES[i], relief="flat", width=style.TAMAÑO_BOTON[0],
+                           height=style.TAMAÑO_BOTON[1], command=None)
+            button.grid(row=style.POSICIONES_OPERADORES[i][0], column=style.POSICIONES_OPERADORES[i][1],
+                        sticky="nsew", padx=2, pady=2)
+            self.buttons.append(button)
+
+    def init_clear(self):
+        """Inicializa el boton CE"""
+        button = tk.Button(self, text="CE", relief="flat", width=style.TAMAÑO_BOTON[0],
+                           height=style.TAMAÑO_BOTON[1], command=lambda:self.erase())
+        button.grid(row=style.POSICION_BORRADOR[0], column=style.POSICION_BORRADOR[1],
+                    sticky="nsew", padx=2, pady=2)
+        self.buttons.append(button)
+
+    def init_equal(self):
+        """Inicializa el boton ="""
+        button = tk.Button(self, text="=", relief="flat", width=1, height=1, command=None)
+        button.grid(row=style.POSICION_IGUAL[0], column=style.POSICION_IGUAL[1],
+                    sticky="nsew", padx=2, pady=2)
+        self.buttons.append(button)
+
+    def write(self, text):
+        """Realiza la función de escribir en la etiqueta al pulsar un botón"""
+        if len(self.text_label) != style.LONGITUD_MAXIMA:
+            self.text_label += str(text)
+            self.etiqueta.config(text=self.text_label)
+
+    def erase(self):
+        """Borra el texto de la etiqueta al completo"""
+        self.etiqueta.config(text="")
+        self.text_label = ""
+
+    def configure_grid(self):
+        """Configura las columnas para que los botones se expandan (6 filas)"""
+        for row in range(style.FILAS):
             self.grid_rowconfigure(row, weight=1)
